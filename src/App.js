f342eb63a21b8ef843714189c426c595b6f02d41
Eduardo Fidelis Chaves <eduardofidelis9@gmail.com>
2023-06-26 16:48:50 -0300

    inserindo o logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './imagens/logobuscabanda.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+
       </header>
+      <body className='App-body'> {/* Tag com logo e searchbar */}
+      <div className ='logo'>  {/* Logo */}
+        <img src={logo}></img>
+      </div>
+
+      </body>
     </div>
   );
 }
