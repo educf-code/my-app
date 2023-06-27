ecad2ee66c3a960ed8283dc60b7c36ab499a8b7f
Eduardo Fidelis Chaves <eduardofidelis9@gmail.com>
2023-06-27 20:15:22 -0300

    estilizando a barra de pesquisa

--- src/componentes/BarradePesquisa/index.js
+++ src/componentes/BarradePesquisa/index.js
@@ -1,9 +1,21 @@
-import './estilo.css'
+import React from 'react';
+import styled from 'styled-components'
+{/* Estiliza a Caixa de Texto */}
+const CaixadeTextoContainer = styled.input`
+    float: right;
+    padding: 6px;
+    font-size: 17px;
+    width: 800px;
+    margin-top: 4%;
+    border-radius: 50px;
+    border: 3px solid rgb(79,144,253);
+
+`
 {/* Cria a Barra de Pesquisa */}
 function BarradePesquisa(){
     return(
         <div className='BarradePesquisa'> {/* Barra de Pesquisa */}
-            <input type='text' placeholder='Pesquisar' className='CaixadeTexto'></input>
+            <CaixadeTextoContainer placeholder='Pesquisar'/>
         </div>
     )
 }
