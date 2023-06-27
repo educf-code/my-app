777a2e082bb4b1e7092c2bbaa9648994e7b54ca0
Eduardo Fidelis Chaves <eduardofidelis9@gmail.com>
2023-06-27 16:54:50 -0300

    componentizando

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import './App.css';
-import BarradePesquisa from './componentes/Barra de pesquisa';
-import Logo from './componentes/Logo'
+import Body from './componentes/Body';
 function App() {
   return (
     <div className='App'>
-      <header>
-
-      </header>
-      <body className='App-body'> {/* Tag com logo e searchbar */}
-        <Logo></Logo> {/* Uso das funções como tags */}
-        <BarradePesquisa></BarradePesquisa>
-      </body>
+      <Body/>
     </div>
   );
 }
