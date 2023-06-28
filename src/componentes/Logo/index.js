1957362608cd3bb6b047546ee659d0c63ffa12d1
Eduardo Fidelis Chaves <eduardofidelis9@gmail.com>
2023-06-28 13:21:12 -0300

    inserindo o link da página inicial no logo

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from '../../imagens/logobuscabanda.png'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 {/* Estiliza o Logo */}
 const LogoContainer = styled.div`
     justify-content: center;
     align-items:center;
+    cursor: pointer;
 `
 {/* Função que mostra o logo */}
 function Logo(){
    return(
     <LogoContainer>
-        <img src={logo} alt='BuscaBanda'></img>
+        <Link to='/'><img src={logo} alt='BuscaBanda'></img></Link>
+        
+        
 
     </LogoContainer>
    ) 
