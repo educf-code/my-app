4ac691299e6c8db17346e81ceac2671fa12ce6b1
Eduardo Fidelis Chaves <eduardofidelis9@gmail.com>
2023-07-07 02:37:03 -0300

    deixando o layout mais responsivo

--- src/componentes/Resposta Youtube/index.js
+++ src/componentes/Resposta Youtube/index.js
@@ -1,17 +1,17 @@
-import BarradePesquisa from "../Pesquisa";
 import React from "react";
 import { styled } from "styled-components";
 import { Link } from "react-router-dom";
+import he from 'he'
 {/* Estilizando o Container que contém as respostas */}
 const ResultadoYoutubeContainer = styled.div`
     display: flex;    
     align-items: stretch;
     align-content:center;
     column-gap: 3%;
-    margin-top: 2%;
+    margin-top: 5%;
     height: fit-content;
     width: fit-content;
-    margin-left: 8%;
+    margin-left: 3%;
     text-decoration: none;
 `
 {/* Estilizando o Container de cada resposta */}
@@ -23,9 +23,9 @@ const ResultadoYoutubeItens = styled.div`
     column-gap: 3%;
     margin-top: 2%;
     height: 100vh;
-    border-radius: 30px;
-    border: 10px solid rgb(79,144,253);
-    padding: 10px;
+    border-radius: 2vw;
+    border: 0.7vw solid rgb(79,144,253);
+    padding: 0.7vw;
     font-family: 'Belanosima';
     color: rgb(107,107,107);
     font-style: normal;
@@ -36,14 +36,14 @@ const ResultadoYoutubeItens = styled.div`
 
     h2{
         align-self: center;
-        height: 96px;
+        height: 6.2vw;
         font-weight: bolder;
         text-decoration: none;
         
     }
     h3{
         align-self: center;
-        height: 100px;
+        height: 6.7vw;
         font-weight: normal;
         text-decoration: none;
     }
@@ -53,35 +53,36 @@ const ResultadoYoutubeItens = styled.div`
     }
     img{
         margin-bottom: 20%;
-        max-height: 180px;
+        max-height: 12vw;
         text-decoration: none;
         max-width: 100%;
 
     
     }
 `
-
-
-{/* Passando pelo array de respostas e obtendo cada item */}
-
-function RespostaAPIYoutube({videos}){
-    return(
-        <ResultadoYoutubeContainer >
-            {videos.map((video) => (
-
-            <Link to={`https://www.youtube.com/watch?v=${video.id.videoId}}`} className ='link'><ResultadoYoutubeItens key={video.id.videoId}>
-               
-                
-                <img  src={video.snippet.thumbnails.medium.url}/>
-                <h2>{video.snippet.title}</h2>
-                <h3>{video.snippet.description}</h3>
-                <p>{video.snippet.channelTitle}</p>
-                
-            </ResultadoYoutubeItens></Link>  
-            ))}
-        </ResultadoYoutubeContainer>
-    )
-        
-        
-}
-export default RespostaAPIYoutube
+function RespostaAPIYoutube({ videos }) {
+    return (
+      <ResultadoYoutubeContainer>
+        {/* Passando pelo array de respostas e obtendo cada item */}
+        {videos.map((video) => {
+        {/* Decodificando os caracteres HTML */}
+          const Título = he.decode(video.snippet.title);
+          const Descrição = he.decode(video.snippet.description);
+          const Canal = he.decode(video.snippet.channelTitle);
+  
+          return (
+            <Link to={`https://www.youtube.com/watch?v=${video.id.videoId}`} className='link' key={video.id.videoId}>
+              <ResultadoYoutubeItens>
+                <img src={video.snippet.thumbnails.medium.url} alt="thumbnail" />
+                <h2>{Título}</h2>
+                <h3>{Descrição}</h3>
+                <p>{Canal}</p>
+              </ResultadoYoutubeItens>
+            </Link>
+          );
+        })}
+              </ResultadoYoutubeContainer>
+    );
+  }
+  
+  export default RespostaAPIYoutube;
